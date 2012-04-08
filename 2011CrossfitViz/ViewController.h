0460e4e46f8371fede3a7459e06f69397baca599
Ryan Rusnak <ryandrusnak@gmail.com>
2012-04-08 11:04:10 -0400

    Initial Commit

---
+++ 2011CrossfitViz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  2011CrossfitViz
+//
+//  Created by Ryan Rusnak on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 2011CrossfitViz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2011CrossfitViz
+//
+//  Created by Ryan Rusnak on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
